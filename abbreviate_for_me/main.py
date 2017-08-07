e54ce6c54eb3ab9b9229f1d0522eff6ac1507494
bruno_messias <messias.physics@gmail.com>
2017-08-07 14:45:02 -0300

    Fix #2

--- abbreviate_for_me/main.py
+++ abbreviate_for_me/main.py
@@ -9,9 +9,55 @@ from bibtexparser.bibdatabase import BibDatabase
 from multiprocessing import Pool
 
 
-# with open("../scrapper/list_abbrev.json") as file_data:
-with open("../scrapper/list_abbrev.json") as file_data:
-    list_abbrev = json.load(file_data)
+class Db_abbrev():
+    def __init__(self, db_file="../scrapper/list_abbrev.json"):
+        self.db_file = db_file
+        self.updates_db = []
+        self.open()
+
+    def open(self):
+        with open(self.db_file) as file_data:
+            self.db = json.load(file_data)
+
+    def insert(self, name, abbrev):
+        return self.updates_db.append({
+            "name": name,
+            "abbrev": abbrev,
+            "what": "insert"
+        })
+
+    def update(self, name, abbrev):
+        return self.updates_db.append({
+            "name": name,
+            "abbrev": abbrev,
+            "what": "update"
+        })
+
+    def get_indexes(self, value, key="name"):
+        def get_items_by_name(item):
+            return item.get(key) == value
+
+        indexes = self.db.index(
+            filter(get_items_by_name, self.db)[0]
+        )
+        return indexes
+
+    def close(self):
+        def action(data):
+            what = data["what"]
+            data.pop("what", None)
+            if what == "insert":
+                self.db.append(data)
+            else:
+                index = self.get_indexes(data["name"])
+                self.db[index]["abbrev"] = data["abbrev"]
+        if len(self.updates_db) > 0:
+            map(action, self.updates_db)
+            with open(self.db_file, "w") as file_data:
+                json.dump(self.db, file_data)
+
+
+db_abbrev = Db_abbrev()
 
 
 def get_status(bib):
@@ -22,15 +68,12 @@ def get_status(bib):
         return item["abbrev"].lower() == bib["journal"].lower()
     bib["_text"] = ""
     bib["_type"] = "out_db"
-    a_journal = filter(is_journal, list_abbrev)
-    a_abbrev = filter(is_abbrev, list_abbrev)
-    # pdb.set_trace()
+    a_journal = filter(is_journal, db_abbrev.db)
+    a_abbrev = filter(is_abbrev, db_abbrev.db)
     if len(a_journal) > 0:
-        # pdb.set_trace()
         bib["_text"] = a_journal[0]["abbrev"]
         bib["_type"] = "expanded"
     elif len(a_abbrev) > 0:
-        # pdb.set_trace()
         bib["_text"] = a_abbrev[0]["name"]
         bib["_type"] = "abreviated"
     return bib
@@ -51,21 +94,6 @@ def manual_update(bibs):
         return manual_update(bibs)
 
 
-# def manual_update_out(bibs):
-    # actions = {
-        # "y": lambda: update_bibs(bibs, custom=True),
-        # "n": lambda: bibs,
-    # }
-    # question = "Replace '{}' ? y(yes)/n(no): "
-    # question = question.format(bibs[0]["journal"])
-    # action = raw_input(question)
-    # # try:
-    # try:
-        # return actions.get(action)()
-    # except TypeError:
-        # return manual_update_out(bibs)
-
-
 def manual_update_out(bibs):
     is_abreviation = raw_input(
         "'{}' is a abreviation?y(yes)n(no): ".format(bibs[0]["journal"])
@@ -77,8 +105,8 @@ def manual_update_out(bibs):
         abreviation = raw_input("Insert abreviation:\n")
         full_name = bibs[0]["journal"]
     else:
-        return update_bibs_out(bibs)
-    list_abbrev.append({"name": full_name, "abbrev": abreviation})
+        return manual_update_out(bibs)
+    db_abbrev.insert(full_name, abreviation)
     for i, bib in enumerate(bibs):
         bibs[i]["journal"] = abreviation
     return bibs
@@ -95,21 +123,26 @@ def update_bibs_out(grouped_bibs):
 
         for key, items in groupby(grouped_bibs, lambda i: i["journal"]):
             grouped_by_journal.append(list(items))
-            updated_bibs = map(manual_update_out, grouped_by_journal)
-            updated_bibs = reduce(lambda a, b: a+b, updated_bibs)
-            return updated_bibs
+        updated_bibs = map(manual_update_out, grouped_by_journal)
+        updated_bibs = reduce(lambda a, b: a+b, updated_bibs)
+        return updated_bibs
     else:
         return update_bibs_out(grouped_bibs)
 
 
 def update_bibs(bibs, custom=False):
     if custom:
-        journal = raw_input("Insert abreviation:\n")
+        abbrev = raw_input("Insert abreviation:\n")
+        db_abbrev.update(
+            bibs[0]["journal"],
+            abbrev
+        )
     else:
-        journal = bibs[0]["_text"]
+        abbrev = bibs[0]["_text"]
 
     for i, bib in enumerate(bibs):
-        bibs[i]["journal"] = journal
+        bibs[i]["journal"] = abbrev
+
     return bibs
 
 
@@ -149,7 +182,6 @@ def main():
     parser.add_argument(
         "--output", "-o",
         required=True,
-        # type=argparse.FileType("w"),
         help="bibtex output file")
 
     args = parser.parse_args()
@@ -182,48 +214,43 @@ def main():
         else:
             bibs_in_db.append(bib)
 
-    # bib_status = map(
-    # lambda group: list(group[1]),
-    # groupby(
-    # map(get_status, bibs_journal),
-    # operator.itemgetter("_type"))
-    # )
-    print "%d bibs are arxiv" % len(bibs_arxiv)
-    print "%d bibs don't have journal tag " % len(bibs_not_journal)
-    print "%d bibs are already abbreviated  " % len(bibs_abreviated[0])
-    print "%d bibs can be easily abbreviated " % len(bibs_in_db[0])
-    print "%d bibs must be manualy abbreviated, "% len(bibs_out_db[0]) + \
-        "at least this time "
-    # pdb.set_trace()
-    updated_bibs = reduce(
-        lambda a, b: a + b,  map(update_bibs_in, bibs_in_db)
-    )
-    updated_bibs += reduce(
-        lambda a, b: a + b, map(update_bibs_out, bibs_out_db)
-    )
-    updated_bibs += bibs_abreviated[0]
+    if len(bibs_arxiv) > 0:
+        print "%d bibs are arxiv" % len(bibs_arxiv)
+        updated_bibs = bibs_arxiv
 
-    # bibs = bibs_in_db
-    # bibs = bibs_out_db + bibs_expanded
-    # pdb.set_trace()
-    [
-        [
-            bib.pop(t, None) for bib in updated_bibs
-        ]
-        for t in ["_text", "_type"]
-    ]
-    updated_bibs += bibs_arxiv + bibs_not_journal
-    # bibtex.entries = bibs
+    if len(bibs_not_journal) > 0:
+        print "%d bibs don't have journal tag " % len(bibs_not_journal)
+
+        updated_bibs += bibs_not_journal
+
+    if len(bibs_abreviated) > 0:
+        print "%d bibs are already abbreviated  " % len(bibs_abreviated[0])
+        updated_bibs += bibs_abreviated[0]
+
+    if len(bibs_in_db) > 0:
+        print "%d bibs can be easily abbreviated " % len(bibs_in_db[0])
+        updated_bibs = reduce(
+            lambda a, b: a + b,  map(update_bibs_in, bibs_in_db)
+        )
+
+    if len(bibs_out_db) > 0:
+        print "%d bibs must be manualy abbreviated, " % len(bibs_out_db[0]) + \
+            "at least this time "
+        updated_bibs += reduce(
+            lambda a, b: a + b, map(update_bibs_out, bibs_out_db)
+        )
+
+    for bib in updated_bibs:
+        if set(("_text", "_type")).issubset(bib) in bib:
+            bib.pop("_text", None)
+            bib.pop("_type", None)
 
     writer = BibTexWriter()
     new_bibtex = BibDatabase()
     new_bibtex.entries = updated_bibs
-    # pdb.set_trace()
     with open(args.output, 'w') as bibfile:
         bibfile.write(writer.write(new_bibtex).encode("utf8"))
-    # bibtexparser.dump(bibtex, args.output.read())
-    # pdb.set_trace()
-    # return bibs
+    db_abbrev.close()
 
 
 if __name__ == "__main__":
