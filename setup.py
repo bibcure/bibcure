476a63b12ba6817b2042dae2b1cf170cf1c42d3d
bruno_messias <messias.physics@gmail.com>
2017-08-09 15:50:39 -0300

    Implements the binary search algorithm... because it's cool,
    Fix #6
    Fix #7
    Fix #3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme.close()
 
 setup(
     name="journalabbrev",
-    version="0.1.2",
+    version="0.1.4",
     packages = find_packages(exclude=["build",]),
     scripts=["journalabbrev/bin/journalabbrev"],
     long_description = README_TEXT,
@@ -19,7 +19,7 @@ setup(
     description="Abbreviates journal names inside in a given bibtex file",
     author="Bruno Messias",
     author_email="contato@brunomessias.com",
-    download_url="https://github.com/devmessias/journalabbrev/archive/0.1.1.tar.gz",
+    download_url="https://github.com/devmessias/journalabbrev/archive/0.1.4.tar.gz",
     keywords=["bibtex", "abbreviate", "science","scientific-journals"],
 
     classifiers=[
